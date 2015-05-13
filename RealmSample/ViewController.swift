6e2101df34ed16994f12cee4dc45060dc5c3fdc9
Satoshi Nagasaka <satoshi.nagasaka21@gmail.com>
2015-05-13 15:37:25 +0900

    Initial Commit

---
+++ RealmSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmSample
+//
+//  Created by Satoshi Nagasaka on 2015/05/13.
+//  Copyright (c) 2015年 SatoshiN21. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
